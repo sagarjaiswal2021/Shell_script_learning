f660c9fe3e624df3ca9e06561ec6b50395e11088
sagarjaiswal2021 <70481766+sagarjaiswal2021@users.noreply.github.com>
2024-01-05 14:19:36 +0530

    Update first_1.sh from GitHub

--- testing_python_script/first_1.sh
+++ testing_python_script/first_1.sh
@@ -2,6 +2,7 @@
 echo "Hello World"
 echo "This is our first shell script"
 echo "Modifying shell file for testing git"
+echo "Performing the changes from git hub portal to practice"
 
 ls
 #cal #calender
